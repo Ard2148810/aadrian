361d561d2fd428d119d6d2468abb9b3a13a6770d
Adrian Ałaszewski <wns2148810@interia.pl>
2021-10-11 15:43:52 +0200

    testing lib usage

--- apps/anymicrob/src/app/app.module.ts
+++ apps/anymicrob/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ArduiModule } from '@aadrian/ardui';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ArduiModule],
   providers: [],
   bootstrap: [AppComponent],
 })
